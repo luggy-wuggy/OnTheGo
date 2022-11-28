20c143f7614f41953faac448b95e7b852ea300a5
Luqman Abdurrohman <luggyman123@gmail.com>
2022-11-27 16:56:54 -0800

    Initial Commit

---
+++ OnTheGo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnTheGo
+//
+//  Created by Luqman Abdurrohman on 11/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
